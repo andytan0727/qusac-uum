22576a31a7f3e83e50e10d09d47a4fc1c628a8e0
Andy Tan <andytan0727@gmail.com>
2019-05-23 23:28:42 +0800

    chore: move contact to contact-us + typescript

--- src/pages/contact-us/thanks.tsx
+++ src/pages/contact-us/thanks.tsx
@@ -5,10 +5,8 @@ export default () => (
   <Layout>
     <section className="section">
       <div className="container">
-        <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
-        </div>
+        <h1>Thank you!</h1>
+        <p>This is a custom thank you page for form submissions</p>
       </div>
     </section>
   </Layout>
