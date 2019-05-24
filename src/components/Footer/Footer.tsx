5fd9629127fe432b4688df83730c67e9d0a6adcf
Andy Tan <andytan0727@gmail.com>
2019-05-24 13:54:11 +0800

    refactor(footer): increase fa icons size

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,37 +4,33 @@ import { FaTwitter, FaFacebookF, FaGithub } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-8 col-md-10 mx-auto">
-            <ul className="list-inline text-center">
-              <li className="list-inline-item">
-                <a href="http://www.twitter.com">
-                  <span className="fa-stack fa-lg">
-                    <FaTwitter />
-                  </span>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="https://www.facebook.com">
-                  <span className="fa-stack fa-lg">
-                    <FaFacebookF />
-                  </span>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="https://www.github.com">
-                  <span className="fa-stack fa-lg">
-                    <FaGithub />
-                  </span>
-                </a>
-              </li>
-            </ul>
-            <p className="copyright text-muted">
-              Copyright &copy; Andy Tan @ UUM Qusac AMC
-            </p>
-          </div>
-        </div>
+      <div className="container mt-5">
+        <ul className="list-inline text-center">
+          <li className="list-inline-item">
+            <a href="http://www.twitter.com">
+              <span>
+                <FaTwitter size={32} />
+              </span>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="https://www.facebook.com">
+              <span>
+                <FaFacebookF size={32} />
+              </span>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="https://www.github.com">
+              <span>
+                <FaGithub size={32} />
+              </span>
+            </a>
+          </li>
+        </ul>
+        <p className="text-muted text-center">
+          Copyright &copy; Andy Tan @ UUM Qusac AMC
+        </p>
       </div>
     </footer>
   );
