cf92c632d10eacd613944600fe360e0be8f5803a
Andy Tan <andytan0727@gmail.com>
2019-06-05 22:41:54 +0800

    refactor: use google form in contact

--- src/pages/contact-us/index.tsx
+++ src/pages/contact-us/index.tsx
@@ -2,78 +2,23 @@ import React from "react";
 import Layout from "../../components/Layout";
 
 const ContactIndex = () => {
-  // TODO: handle submit
-  const handleSubmit = (e: HTMLFormElement) => {
-    e.preventDefault();
-  };
-
   return (
     <Layout>
       <div className="container mt-5">
         <h1>Contact Us</h1>
-        <div className="row mt-5">
-          <div className="col-lg-12">
-            <form id="contactForm" name="sentMessage">
-              <div className="row">
-                <div className="col-md-6">
-                  <div className="form-group">
-                    <input
-                      className="form-control"
-                      id="name"
-                      type="text"
-                      placeholder="Your Name *"
-                      required
-                      data-validation-required-message="Please enter your name."
-                    />
-                    <p className="help-block text-danger" />
-                  </div>
-                  <div className="form-group">
-                    <input
-                      className="form-control"
-                      id="email"
-                      type="email"
-                      placeholder="Your Email *"
-                      required
-                      data-validation-required-message="Please enter your email address."
-                    />
-                    <p className="help-block text-danger" />
-                  </div>
-                  <div className="form-group">
-                    <input
-                      className="form-control"
-                      id="phone"
-                      type="tel"
-                      placeholder="Your Phone *"
-                      data-validation-required-message="Please enter your phone number."
-                    />
-                    <p className="help-block text-danger" />
-                  </div>
-                </div>
-                <div className="col-md-6">
-                  <div className="form-group">
-                    <textarea
-                      className="form-control"
-                      id="message"
-                      placeholder="Your Message *"
-                      required
-                      data-validation-required-message="Please enter a message."
-                    />
-                    <p className="help-block text-danger" />
-                  </div>
-                </div>
-                <div className="clearfix" />
-                <div className="col-lg-12 text-center">
-                  <div id="success" />
-                  <button
-                    id="sendMessageButton"
-                    className="btn btn-primary btn-xl text-uppercase"
-                    type="submit"
-                  >
-                    Send Message
-                  </button>
-                </div>
-              </div>
-            </form>
+        <div
+          className="embed-responsive embed-responsive-16by9 mt-5"
+          style={{
+            minHeight: "65vh",
+          }}
+        >
+          <div className="embed-responsive-item">
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLSdUT-D6STH2PDtDdwFsbKStaCvw81ZkE_uqCeGQGA-pU0o01Q/viewform?embedded=true"
+              frameBorder="0"
+            >
+              Loading...
+            </iframe>
           </div>
         </div>
       </div>
