c21a25d583136e9025a21e2f57b7fcb0798825e0
Andy Tan <andytan0727@gmail.com>
2019-06-12 21:08:52 +0800

    refactor(news): eliminate duplicate code

--- src/components/NewsRoll/NewsRoll.tsx
+++ src/components/NewsRoll/NewsRoll.tsx
@@ -3,7 +3,7 @@ import { Link, navigate } from "gatsby";
 import { FaArrowRight } from "react-icons/fa";
 import { Card, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
 import { useNewsRollQuery } from "../StaticQueries/NewsRollQuery";
-import { Zoom, Fade } from "react-reveal";
+import { Fade } from "react-reveal";
 
 export interface NewsRollProps {
   shortPosts?: boolean;
@@ -11,7 +11,8 @@ export interface NewsRollProps {
 
 const NewsRoll = (props: NewsRollProps) => {
   const { shortPosts } = props;
-  const { edges: newsPosts } = useNewsRollQuery();
+  const { edges } = useNewsRollQuery();
+  const newsPosts = !shortPosts ? edges : edges.slice(0, 3);
 
   const handleNavigateToNews = () => {
     navigate("/news");
@@ -19,69 +20,36 @@ const NewsRoll = (props: NewsRollProps) => {
 
   return (
     <React.Fragment>
-      {!shortPosts ? (
-        newsPosts.map(({ node: post }) => (
-          <Fade key={post.id}>
-            <Card>
-              <CardBody>
-                <CardTitle>
-                  <Link
-                    className="h3 font-weight-bold"
-                    to={`${post.fields.slug}`}
-                  >
-                    {post.frontmatter.title}
-                  </Link>
-                </CardTitle>
-                <CardSubtitle>Created at {post.frontmatter.date}</CardSubtitle>
-                <CardText>{post.excerpt}</CardText>
-                <Link className="btn btn-outline-dark" to={post.fields.slug}>
-                  <span className="d-inline-flex align-items-center">
-                    Read more <FaArrowRight className="ml-1" />
-                  </span>
+      {newsPosts.map(({ node: post }) => (
+        <Fade key={post.id}>
+          <Card>
+            <CardBody>
+              <CardTitle>
+                <Link
+                  className="h3 font-weight-bold"
+                  to={`${post.fields.slug}`}
+                >
+                  {post.frontmatter.title}
                 </Link>
-              </CardBody>
-            </Card>
-          </Fade>
-        ))
-      ) : (
-        <div>
-          {newsPosts.slice(0, 3).map(({ node: post }) => {
-            return (
-              <Zoom key={post.id}>
-                <Card>
-                  <CardBody>
-                    <CardTitle>
-                      <Link
-                        className="h3 font-weight-bold"
-                        to={`${post.fields.slug}`}
-                      >
-                        {post.frontmatter.title}
-                      </Link>
-                    </CardTitle>
-                    <CardSubtitle>
-                      Created at {post.frontmatter.date}
-                    </CardSubtitle>
-                    <CardText>{post.excerpt}</CardText>
-                    <Link
-                      className="btn btn-outline-dark"
-                      to={post.fields.slug}
-                    >
-                      <span className="d-inline-flex align-items-center">
-                        Read more <FaArrowRight className="ml-1" />
-                      </span>
-                    </Link>
-                  </CardBody>
-                </Card>
-              </Zoom>
-            );
-          })}
-          <button
-            className="btn btn-primary mt-4 mb-5"
-            onClick={handleNavigateToNews}
-          >
-            Read More on News
-          </button>
-        </div>
+              </CardTitle>
+              <CardSubtitle>Created at {post.frontmatter.date}</CardSubtitle>
+              <CardText>{post.excerpt}</CardText>
+              <Link className="btn btn-outline-dark" to={post.fields.slug}>
+                <span className="d-inline-flex align-items-center">
+                  Read more <FaArrowRight className="ml-1" />
+                </span>
+              </Link>
+            </CardBody>
+          </Card>
+        </Fade>
+      ))}
+      {shortPosts && (
+        <button
+          className="btn btn-primary mt-4 mb-5"
+          onClick={handleNavigateToNews}
+        >
+          Read More on News
+        </button>
       )}
     </React.Fragment>
   );
